05ea28fdfb98c537d978755604260746076adc12
liji3597 <asd1020205749@foxmail.com>
2026-02-03 16:44:22 +0800

    depoly practice

---
+++ Remix/practice8.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract testArray {
+    // storage 位置
+    uint[] public arr1 = new uint[](1);
+
+    function add(uint x) public {
+        arr1.push(x);
+    }
+
+    function arr1Len() public view returns (uint len) {
+        return arr1.length;
+    }
+}
---
+++ Remix/practice9.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract StringUtils {
+    // TODO: 实现字符串拼接
+    function concat(string memory a, string memory b)
+        public
+        pure
+        returns (string memory)
+    {
+        // 你的代码
+    }
+
+    // TODO: 判断字符串是否为空
+    function isEmpty(string memory str) public pure returns (bool) {
+        // 你的代码
+    }
+
+    // TODO: 获取字符串长度
+    function length(string memory str) public pure returns (uint) {
+        // 你的代码
+    }
+}
